35103bbe36a84c862500d7465f86e3ce6837cb76
Tangerine Cat <kaineer@gmail.com>
2016-06-17 17:52:21 +0500

    Implemented module1-task2

--- build/js/check.js
+++ build/js/check.js
@@ -1,4 +1,6 @@
 function getMessage(a, b) {
+  var i, sum;
+
   if(typeof(a) === 'boolean') {
     if(a) {
       return 'Я попал в ' + b;
@@ -9,5 +11,21 @@ function getMessage(a, b) {
 
   if(typeof(a) === 'number') {
     return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+  }
+
+  if(a) {
+    if(b) {
+      for(i = 0, sum = 0; i < a.length; i++) {
+        sum += a[i] * b[i];
+      }
+
+      return 'Я прошёл ' + sum + ' метров';
+    } else {
+      for(i = 0, sum = 0; i < a.length; i++) {
+        sum += a[i];
+      }
+
+      return 'Я прошёл ' + sum + ' шагов';
+    }
   }
 }
