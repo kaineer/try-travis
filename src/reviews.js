7bc09ae7c72a9e2ee5a27d25ca0e12d2965c5cbb
Tangerine Cat <kaineer@gmail.com>
2016-06-28 10:46:00 +0500

    [ci skip] Fix reviews.js back

--- src/reviews.js
+++ src/reviews.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// var getReviews = require('./reviews/get-reviews');
-// var displayReviews = require('./reviews/display-reviews');
+var getReviews = require('./reviews/get-reviews');
+var displayReviews = require('./reviews/display-reviews');
 
-// getReviews(displayReviews);
+getReviews(displayReviews);
